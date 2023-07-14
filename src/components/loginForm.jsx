5950d6542628a02ad5ae2aa85151018e44655185
Juanjo Asensio García <asengar2009@gmail.com>
2023-07-15 00:17:58 +0200

    Add a credential error alert

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,9 +1,11 @@
 import { useState, React } from "react";
-import { Container, FloatingLabel, Form, Button } from "react-bootstrap";
+import { Container, FloatingLabel, Form, Button, Alert } from "react-bootstrap";
 
 import { login } from "../db";
 
 export function LoginForm({ t }) {
+    const [credentialError, setCredentialError] = useState(false);
+
     const [passwordVisibility, setPasswordVisibility] = useState({
         status: "password",
         icon: "bi bi-eye-fill",
@@ -32,19 +34,24 @@ export function LoginForm({ t }) {
 
     const [validated, setValidated] = useState(false);
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         const form = event.currentTarget;
         event.preventDefault();
 
         if (form.checkValidity() === false) {
             event.stopPropagation();
         } else {
-            login("asengar2009@gmail.com", "prueba");
+            setCredentialError(false);
 
+            const user = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
+
+            if (await login(user, password)) {
+                setCredentialError(true);
+            }
         }
 
         setValidated(true);
-
     };
 
     const url = new URL(window.location.href);
@@ -73,47 +80,60 @@ export function LoginForm({ t }) {
     redirectName = t("logIn.goto") + redirectName;
 
     return (
-        <Container className="d-flex flex-wrap justify-content-center justify-content-xl-start h-100 pt-5 mb-5">
-            <div className="w-100 align-self-end pt-1 pt-md-4 pb-4" style={{ maxWidth: 526 }}>
-                <h1 className="text-center text-xl-start">{t("logIn.welcome")}</h1>
-                <h6>{redirectName}</h6>
+        <>
+            <CredentialsError t={t} show={credentialError} setShow={setCredentialError} />
+            <Container className="d-flex flex-wrap justify-content-center justify-content-xl-start h-100 pt-5 mb-5">
+                <div className="w-100 align-self-end pt-1 pt-md-4 pb-4" style={{ maxWidth: 526 }}>
+                    <h1 className="text-center text-xl-start">{t("logIn.welcome")}</h1>
+                    <h6>{redirectName}</h6>
 
-                <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                    <FloatingLabel
-                        controlId="floatingInput"
-                        label={t("email")}
-                        className="mb-3"
-                    >
-                        <Form.Control required type="email" placeholder="" />
-                        <Form.Control.Feedback>{t("verify.ok")}</Form.Control.Feedback>
-                        <Form.Control.Feedback type="invalid">{t("verify.email")}</Form.Control.Feedback>
-                    </FloatingLabel>
+                    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                        <FloatingLabel
+                            controlId="email"
+                            label={t("email")}
+                            className="mb-3"
+                        >
+                            <Form.Control required type="email" placeholder="" />
+                            <Form.Control.Feedback>{t("verify.ok")}</Form.Control.Feedback>
+                            <Form.Control.Feedback type="invalid">{t("verify.email")}</Form.Control.Feedback>
+                        </FloatingLabel>
 
-                    <FloatingLabel controlId="floatingPassword" label={t("password")}>
-                        <Form.Control required type={passwordVisibility.status} placeholder="" />
+                        <FloatingLabel controlId="password" label={t("password")}>
+                            <Form.Control required type={passwordVisibility.status} placeholder="" />
 
-                        <div style={{position: "relative"}}>
-                            <button className="password-toggle" type="button" onClick={changePasswordVisibility}>
-                                <i className={passwordVisibility.icon}></i>
-                            </button>
-                        </div>
+                            <div style={{ position: "relative" }}>
+                                <button className="password-toggle" type="button" onClick={changePasswordVisibility}>
+                                    <i className={passwordVisibility.icon}></i>
+                                </button>
+                            </div>
 
-                        <Form.Control.Feedback>{t("verify.ok")}</Form.Control.Feedback>
-                        <Form.Control.Feedback type="invalid">{t("verify.password")}</Form.Control.Feedback>
-                    </FloatingLabel>
+                            <Form.Control.Feedback>{t("verify.ok")}</Form.Control.Feedback>
+                            <Form.Control.Feedback type="invalid">{t("verify.password")}</Form.Control.Feedback>
+                        </FloatingLabel>
 
-                    <Button variant="primary" type="submit" className="mt-3 mb-3 w-100">{t("logIn.logIn")}</Button>
-                </Form>
-                <a className="btn w-100" href="#">
-                    {t("logIn.forgotten")}
-                </a>
+                        <Button variant="primary" type="submit" className="mt-3 mb-3 w-100">{t("logIn.logIn")}</Button>
+                    </Form>
+                    <a className="btn w-100" href="#">
+                        {t("logIn.forgotten")}
+                    </a>
 
-                <hr className="my-4" />
-                <p className="text-center text-xl-start pb-3 mb-3">
-                    {t("logIn.notAccountYet")}
-                    <a href="account-signup.html">{t("logIn.register")}</a>
-                </p>
-            </div>
-        </Container >
+                    <hr className="my-4" />
+                    <p className="text-center text-xl-start pb-3 mb-3">
+                        {t("logIn.notAccountYet")}
+                        <a href="account-signup.html">{t("logIn.register")}</a>
+                    </p>
+                </div>
+            </Container >
+        </>
     );
+}
+
+function CredentialsError({ t, show, setShow }) {
+    if (show) {
+        return (
+            <Alert className="mt-2" variant="danger" onClose={() => setShow(false)} dismissible>
+                User or password invalid!
+            </Alert>
+        );
+    }
 }
