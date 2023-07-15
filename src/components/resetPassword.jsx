b9e23d9df3f24a6a446c2a212cfbaa4488a4dd54
Juanjo Asensio García <asengar2009@gmail.com>
2023-07-15 16:23:13 +0200

    Translate EmailSended

--- src/components/resetPassword.jsx
+++ src/components/resetPassword.jsx
@@ -95,9 +95,6 @@ function SendEmail({ t, setStatus }) {
 }
 
 function EmailSended({ t }) {
-    // Show an error
-    const [credentialError, setCredentialError] = useState(false);
-
     return (
         <Container className="d-flex flex-wrap justify-content-center justify-content-xl-start h-100 pt-5 mb-5">
             <div className="w-100 align-self-end pt-1 pt-md-4 pb-4" style={{ maxWidth: 526 }}>
