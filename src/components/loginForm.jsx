00722ad2b64ece5b79deb915af7a0240f506081c
Juanjo Asensio García <asengar2009@gmail.com>
2023-08-03 14:55:50 +0200

    Translate Loading to Spanish

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -7,9 +7,9 @@ const Loading = () => {
     return (
         <div className="position-absolute top-50 start-50 translate-middle">
             <Spinner animation="border" role="status">
-                <span className="visually-hidden">Loading...</span>
+                <span className="visually-hidden">{t("loading")}</span>
             </Spinner>
-            <h3 className="text-center">Loading...</h3>
+            <h3 className="text-center">{t("loading")}</h3>
         </div>
     );
 };
