fa25372402526f21374b36db3c8900bad9cf2c0a
Juanjo Asensio García <asengar2009@gmail.com>
2023-08-03 19:39:35 +0200

    Add the org's ico to the logOut page

--- src/components/logout.jsx
+++ src/components/logout.jsx
@@ -11,6 +11,7 @@ export function Logout({ t }) {
 
     return (
         <div className="position-absolute top-50 start-50 translate-middle text-center">
+            <img src="/login/images/favicon_org.ico" alt="Logo" className="d-inline-block align-text-middle" />
             <h1>{t("logOut.thanks")}</h1>
             <h4>
                 {t("logOut.logInAgain")}
